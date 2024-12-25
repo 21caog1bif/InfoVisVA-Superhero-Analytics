ce9ca56b350db22747f213baf4a08d9e73d313b5
SinanPercen <101935658+SinanPercen@users.noreply.github.com>
2024-12-25 22:11:18 +0100

    fix bubble

--- tab5.js
+++ tab5.js
@@ -34,9 +34,9 @@ function updateBubbleChart() {
     bubbleContainer.selectAll("*").remove();
 
     // Chart-Dimensionen
-    const margin = { top: 50, right: 100, bottom: 50, left: 60 };
-    const width = 800 - margin.left - margin.right;
-    const height = 600 - margin.top - margin.bottom;
+    const margin = { top: 80, right: 100, bottom: 80, left: 50 };
+    const width = 1850 - margin.left - margin.right;
+    const height = 1050 - margin.top - margin.bottom;
 
     // Tooltip erstellen
     const tooltip = d3.select("body")
